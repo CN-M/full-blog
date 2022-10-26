3684e3ff872788f4d8dce3793dd1a450bbdaeea9
C.N <cn.mbhalati@gmail.com>
2022-10-26 13:46:37 +0200

    Add posts to home page

---
+++ frontend/src/components/Posts/Posts.jsx
@@ -0,0 +1,15 @@
+import './Posts.scss';
+
+import PostCard from '../PostCard/PostCard';
+
+const Posts = () => (
+  <div>
+    <PostCard />
+    <PostCard />
+    <PostCard />
+    <PostCard />
+    <PostCard />
+  </div>
+);
+
+export default Posts;
--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,9 +1,12 @@
 import './Home.scss';
 
+import Posts from '../../components/Posts/Posts';
+
 function Home() {
   return (
     <div className="container">
       <h1>Welcome to the blog</h1>
+      <Posts />
     </div>
   );
 }
